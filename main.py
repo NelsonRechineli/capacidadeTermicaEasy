75318cc3b8fb7f89c3c926eba3823661f07c5a9e
NelsonRechineli <69924074+NelsonRechineli@users.noreply.github.com>
2024-05-15 12:15:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#quantidade de calor
+quantidadeCalor = 400 
+
+#delta = final - inicial
+variacaoTemperatura = 40-30
+
+#capacidade térmica = quantidade de calor/variação da temperatura
+capacidadeTermica = quantidadeCalor / variacaoTemperatura
+
+print(f"A capacidade térmica do corpo foi de : {capacidadeTermica:,.2f} calorias por grau Celsius (cal/Cº)")
